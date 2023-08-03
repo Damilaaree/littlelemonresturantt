ae3f503643dc5ade2de5ab6bd88b8d8276c309bb
Damilare <121191782+Damilaaree@users.noreply.github.com>
2023-08-03 11:24:45 +0800

    Update OurStory.js

--- src/components/pages/Home/OurStory.js
+++ src/components/pages/Home/OurStory.js
@@ -9,19 +9,11 @@ const OurStory = () => {
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-          culpa qui officia deserunt mollit anim id est laborum. Sed ut 
-          perspiciatis unde omnis iste natus error sit voluptatem accusantium 
-          doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo 
-          inventore veritatis et quasi architecto beatae vitae dicta sunt 
-          explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut 
-          odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
-          voluptatem sequi nesciunt.
+        Located in Chicago, little lemon provides you with with the best
+              dishes around the world.Have relative birthday that you want to
+              celebrate? do you want to experience food like you never did
+              before? reserve a table now, or order online. Little lemon is the
+              best place for your most delicious needs!
         </p>
       </div>
       <div className="our-story-chefs">
